337fa229515f372d6c3f1dc65a733cf48b186554
Rob Cranfill <robcranfill@gmail.com>
2022-05-12 10:16:19 -0700

    Gave up and hardcoded cd

--- run_at_start.sh
+++ run_at_start.sh
@@ -5,11 +5,9 @@
 pid_file="sr18changer.pid"
 
 
-# We need to cd to 'here' so the data file can be found,
-# but I can't figure out how to do that in a smart way.
-# So make sure *you* set the current directory appropropriately.
+# GRRR. How to do this other than hardcoded? It's not clear.
 #
-# cd /home/pi/proj/whatIsThisDirectory?/
+cd /home/pi/proj/piAlesisSR/
 
 source ./setup.sh
 python3 ./sr18_changer.py &
